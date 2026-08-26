7da28524df61d59bc37a76d66415438e82258225
Gustavo Santiago Gremes <gugremes@gmail.com>
2026-08-25 22:25:06 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,185 @@
+import os
+import datetime
+
+mensagens = []
+transacoes = []
+
+nome = input("Nome: ")
+
+def atribuirReceita(valor, local):
+    transacoes.append({
+        "tipo": "receita",
+        "valor": valor,
+        "local": local,
+        "data": datetime.datetime.now()
+    })
+
+def atribuirDespesa(valor, local):
+    transacoes.append({
+        "tipo": "despesa",
+        "valor": valor,
+        "local": local,
+        "data": datetime.datetime.now()
+    })
+
+def calcularSaldo(mes, ano):
+    totalReceita = 0
+    totalDespesa = 0
+    
+    for transacao in transacoes:
+        if transacao["tipo"] == "receita" and transacao["data"].month == mes and transacao["data"].year == ano:
+            totalReceita += transacao["valor"]
+
+        elif transacao["tipo"] == "despesa" and transacao["data"].month == mes and transacao["data"].year == ano:
+            totalDespesa += transacao["valor"]
+        
+    saldo = totalReceita - totalDespesa
+    return saldo, totalDespesa, totalReceita
+
+def gerarDetalhes(mes, ano):
+
+    detalhes_receita = ""
+    detalhes_despesa = ""
+
+    for transacao in transacoes:
+        if transacao["tipo"] == "receita" and transacao["data"].month == mes and transacao["data"].year == ano:
+            detalhes_receita += f'{transacao["local"]} - R$ {transacao["valor"]:.2f}\n'
+
+        elif transacao["tipo"] == "despesa" and transacao["data"].month == mes and transacao["data"].year == ano:
+            detalhes_despesa += f'{transacao["local"]} - R$ {transacao["valor"]:.2f}\n'
+
+    return detalhes_receita, detalhes_despesa
+
+def calcularPorcentagens(mes, ano):
+    gastos_por_local = {}
+    detalhes_porcentagem = ""
+
+    for transacao in transacoes:
+        if transacao["tipo"] == "despesa" and transacao["data"].month == mes and transacao["data"].year == ano:
+            if transacao["local"] in gastos_por_local:
+                gastos_por_local[transacao["local"]] = gastos_por_local[transacao["local"]] + transacao["valor"]
+            else:
+                gastos_por_local[transacao["local"]] = transacao["valor"]
+
+    porcentagens = {}
+    totalDespesa = 0
+    for local in gastos_por_local:
+        valor = gastos_por_local[local]
+        totalDespesa += valor
+
+    for local in gastos_por_local:
+        valor = gastos_por_local[local]
+        porcentagens = (valor / totalDespesa) * 100
+        detalhes_porcentagem += f'{local} - R$ {valor:.2f} - {porcentagens:.2f}%\n'
+    return detalhes_porcentagem
+        
+
+def pedirValor():
+    while True:
+        try: # Condição para não aceitar letras ou algo diferente de números.
+            valor = float(input("Digite a movimentação desse mês: "))
+            if valor <= 0: # Condição para aceitar apenas número positivo.
+                print("Digite apenas números positivos e maiores que zero!")
+                continue
+            break
+        except ValueError:
+            print("Digite apenas números!")
+    return valor
+
+while True:
+
+    os.system("cls") #limpar o terminal sempre que rodar o programa.
+    bot = "@Bot"
+    msg_bot = f"""
+Olá, {nome}! Vamos controlar suas finanças?
+    
+Digite:
+1 - Adicionar receita
+2 - Adicionar despesa
+3 - Relatório
+"""
+    mensagens.append({
+        "nome": bot,
+        "texto": msg_bot
+    })
+    
+    if len(mensagens) > 0:
+        for m in mensagens:
+            print(m['nome'], "-", m['texto'])
+
+    print('________________')
+    
+    texto = input("mensagem: ").lower().strip()
+    if texto == ("fim"):
+        break
+
+        # Salvando a mensagem e o nome no array.
+    mensagens.append({
+        "nome": nome,
+        "texto": texto 
+    })
+
+    match texto:
+        case "1":
+            valor = pedirValor()
+            local = (input("De onde veio essa receita: "))
+            atribuirReceita(valor, local)
+
+            msg_bot = f"Receita de R${valor:.2f} adicionado!"
+
+            mensagens.append({
+                "nome": bot,
+                "texto": msg_bot
+            })
+        case "2":
+            valor = pedirValor()
+            local = (input("de onde veio essa despesa: "))
+            atribuirDespesa(valor, local)
+            
+            msg_bot = f"Despesa de R${valor:.2f} adicionada!"
+            mensagens.append({
+                "nome": bot,
+                "texto": msg_bot
+            })
+        case "3":
+            while True:
+                try:
+                    mes = int(input("Digite o mês: "))
+                    ano = int(input("Digite o ano: "))
+                    if 1 <= mes <= 12:
+                        break
+                    print("Digite um mês válido!")
+                    continue
+                except ValueError:
+                    print("Digite apenas números!")
+            saldo, totalDespesa, totalReceita = calcularSaldo(mes, ano)
+            detalhes_receita, detalhes_despesa = gerarDetalhes(mes, ano)
+            detalhes_porcentagem = calcularPorcentagens(mes, ano)
+
+            msg_bot = f'''
+===== RELATÓRIO DE {mes} / {ano} =====
+
+Receita Total = R$ {totalReceita:.2f}
+Receitas:
+{detalhes_receita}
+
+Despesas Total = R$ {totalDespesa:.2f}
+Gastos:
+{detalhes_despesa}
+
+Saldo Final = R$ {saldo:.2f}
+
+===============================
+Porcentagem de gastos:
+{detalhes_porcentagem}
+'''
+            mensagens.append({
+                "nome": bot,
+                "texto": msg_bot
+            })
+        case _:
+            msg_bot = "não entendi o que você digitou"
+            mensagens.append({
+                "nome": bot,
+                "texto": msg_bot
+            })
